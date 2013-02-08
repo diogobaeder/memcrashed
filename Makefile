65803d05299b5e289e2dfc7d346a73b742662318
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-02-08 19:31:24 -0200

    Initial structure

---
+++ Makefile
@@ -0,0 +1,6 @@
+test:
+	@env PYTHONHASHSEED=random PYTHONPATH=. nosetests --with-coverage --cover-package=memcrashed --cover-erase --with-yanc --with-xtraceback tests/
+
+build: test
+	@echo Running syntax check...
+	@flake8 . --ignore=E501
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+version = '0.1.0-dev'
+
+
+setup(name='memcrashed',
+      version=version,
+      description="A Memcached sharding and failover proxy",
+      long_description="""\
+A Memcached sharding and failover proxy""",
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='memcashed proxy sharding failover',
+      author='Diogo Baeder',
+      author_email='contato@diogobaeder.com.br',
+      url='https://github.com/diogobaeder/memcrashed',
+      license='BSD 2-Clause',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'tornado',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
