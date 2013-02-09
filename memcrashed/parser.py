e0cc4f4d4867fba5fad0ecc31da4f30b31bf1e67
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-02-09 03:56:04 -0200

    Unpacking response header

--- memcrashed/parser.py
+++ memcrashed/parser.py
@@ -2,15 +2,26 @@ from collections import namedtuple
 from struct import Struct
 
 REQUEST_BLOCKS = 'magic opcode key_length extra_length data_type reserved total_body_length opaque cas'
+RESPONSE_BLOCKS = 'magic opcode key_length extra_length data_type status total_body_length opaque cas'
 HEADER_FORMAT = '! B B H B B H I I Q'
 
 
 RequestHeader = namedtuple('RequestHeader', 'raw %s' % REQUEST_BLOCKS)
+ResponseHeader = namedtuple('ResponseHeader', 'raw %s' % RESPONSE_BLOCKS)
 header_struct = Struct(HEADER_FORMAT)
 
 
 def unpack_request_header(header_bytes):
+    fields = extract_fields_for_header(header_bytes)
+    return RequestHeader(*fields)
+
+
+def unpack_response_header(header_bytes):
+    fields = extract_fields_for_header(header_bytes)
+    return ResponseHeader(*fields)
+
+
+def extract_fields_for_header(header_bytes):
     tokens = header_struct.unpack(header_bytes)
     fields = (header_bytes, ) + tokens
-    header = RequestHeader(*fields)
-    return header
+    return fields
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -10,9 +10,9 @@ class ParserTest(TestCase):
     def unpacks_request_header_with_raw_field(self):
         request_bytes = b'\x80\x01\x00\x03\x08\x00\x00\x00\x00\x00\x00\x0e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
 
-        request_header = parser.unpack_request_header(request_bytes)
+        header = parser.unpack_request_header(request_bytes)
 
-        self.assertEqual(request_header.raw, request_bytes)
+        self.assertEqual(header.raw, request_bytes)
 
     @istest
     def unpacks_request_header_with_additional_fields(self):
@@ -26,6 +26,30 @@ class ParserTest(TestCase):
         self.assertEqual(header.extra_length, 0x08)
         self.assertEqual(header.data_type, 0x00)
         self.assertEqual(header.reserved, 0x0000)
+        self.assertEqual(header.total_body_length, 0x0000000e)
+        self.assertEqual(header.opaque, 0x00000000)
+        self.assertEqual(header.cas, 0x0000000000000000)
+
+    @istest
+    def unpacks_response_header_with_raw_field(self):
+        response_bytes = b'\x80\x01\x00\x03\x08\x00\x00\x00\x00\x00\x00\x0e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+        header = parser.unpack_response_header(response_bytes)
+
+        self.assertEqual(header.raw, response_bytes)
+
+    @istest
+    def unpacks_response_header_with_additional_fields(self):
+        response_bytes = b'\x80\x01\x00\x03\x08\x00\x00\x00\x00\x00\x00\x0e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+
+        header = parser.unpack_response_header(response_bytes)
+
+        self.assertEqual(header.magic, 0x80)
+        self.assertEqual(header.opcode, 0x01)
+        self.assertEqual(header.key_length, 0x0003)
+        self.assertEqual(header.extra_length, 0x08)
+        self.assertEqual(header.data_type, 0x00)
+        self.assertEqual(header.status, 0x0000)
         self.assertEqual(header.total_body_length, 0x0000000e)
         self.assertEqual(header.opaque, 0x00000000)
         self.assertEqual(header.cas, 0x0000000000000000)
