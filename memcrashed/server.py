e1f56b40ac68f875aa0cf50b505b88d1e82aada2
Diogo Baeder de Paula Pinto <diogobaeder@yahoo.com.br>
2013-02-10 00:08:26 -0200

    First working server and integration test

---
+++ memcrashed/server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import socket
+
+from tornado import gen, iostream
+from tornado.ioloop import IOLoop
+from tornado.netutil import TCPServer
+
+from memcrashed import parser
+
+
+HEADER_BYTES = 24
+
+
+class Server(TCPServer):
+
+    @gen.engine
+    def handle_stream(self, stream, address):
+        header_bytes = yield gen.Task(stream.read_bytes, HEADER_BYTES)
+        headers = parser.unpack_request_header(header_bytes)
+        body_bytes = yield gen.Task(stream.read_bytes, headers.total_body_length)
+        all_bytes = header_bytes + body_bytes
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        backend = iostream.IOStream(s, io_loop=self.io_loop)
+        yield gen.Task(backend.connect, ("localhost", 11211))
+
+        yield gen.Task(backend.write, all_bytes)
+
+        header_bytes = yield gen.Task(backend.read_bytes, HEADER_BYTES)
+        headers = parser.unpack_response_header(header_bytes)
+        body_bytes = yield gen.Task(backend.read_bytes, headers.total_body_length)
+        all_bytes = header_bytes + body_bytes
+
+        yield gen.Task(stream.write, all_bytes)
+
+
+if __name__ == '__main__':  # pragma: no cover
+    server = Server()
+    server.listen(8888)
+    IOLoop.instance().start()
---
+++ tests/test_server.py
@@ -0,0 +1,49 @@
+import socket
+
+from nose.tools import istest
+from tornado import iostream
+from tornado.testing import AsyncTestCase
+
+from memcrashed.server import Server
+
+
+class ServerTest(AsyncTestCase):
+    @istest
+    def reads_back_a_written_value(self):
+        '''
+        This test uses low-level sockets to check if the Server is respecting the protocol when setting a value to memcached.
+
+        The operation being done is setting "foo" key as "bar" value.
+        '''
+        host = '127.0.0.1'
+        port = 8888
+
+        request_header_bytes = b'\x80\x0c\x00\x03\x00\x00\x00\x00\x00\x00\x00\x03\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        request_body_bytes = b'foo'
+        request_bytes = request_header_bytes + request_body_bytes
+
+        response_header_bytes = b'\x81\x0c\x00\x03\x04\x00\x00\x00\x00\x00\x00\n\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01'
+        response_body_bytes = b'\x00\x00\x00\x00foobar'
+        response_bytes = response_header_bytes + response_body_bytes
+
+        server = Server(io_loop=self.io_loop)
+        server.listen(port, address=host)
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        stream = iostream.IOStream(s, io_loop=self.io_loop)
+
+        def start_test():
+            stream.connect((host, port), send_request)
+
+        def send_request():
+            stream.write(request_bytes)
+            stream.read_bytes(len(response_bytes), receive_response)
+
+        def receive_response(data):
+            self.assertEqual(data, response_bytes)
+            stream.close()
+            self.stop()
+
+        self.io_loop.add_callback(start_test)
+
+        self.wait()
